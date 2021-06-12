676feb9fe8025ba20db481bfe0f80c9e42986ef8
Gottamn <nomail@nomail.com>
2021-06-12 13:56:08 +0300

    Add task #2

--- LINQ2/Program.cs
+++ LINQ2/Program.cs
@@ -8,13 +8,13 @@ namespace LINQ2
     {
         static void Main(string[] args)
         {
-            IEnumerable<int> numbers = new int[] { 2, 3, 8, -1, 0, 6 };
+            IEnumerable<int> numbers = new int[] {2, 3, 8, -1, 0, 6 };
 
             // 1.	Проверить, есть ли в последовательности элементы больше чем заданное с консоли значение.
 
             if (int.TryParse(Console.ReadLine(), out var readStr))
             {
-                if (numbers.Any(n => n > readStr))
+                if (numbers?.Any(n => n > readStr) ?? false)
                 {
                     Console.WriteLine("Ok");
                 }
@@ -28,8 +28,17 @@ namespace LINQ2
                 Console.WriteLine("Incorrect value");
             }
 
+            // 2.	Найти сумму всех элементов последовательности, предшествующих первому нулевому элементу.
+            // Если по какой-либо причине вычислить сумму не удается, выдать об этом сообщение с указанием причины.
 
-
+            if (numbers == null)
+            {
+                Console.WriteLine("Empty sequence");
+            }
+            else
+            {
+                Console.WriteLine(numbers.TakeWhile(n => n != 0).Sum()); 
+            }
 
         }
     }
